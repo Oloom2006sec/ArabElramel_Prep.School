f2e22384ac2bd2e25b3f2c8c3cc59ef71d9e0ab4
Oloom2006sec <oloom2006@outlook.com>
2025-06-04 22:32:55 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,18 @@
-const CACHE_NAME = 'results-cache-v1';
+const CACHE_NAME = 'v1';
+
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  'index.html',
+  'style.css',
+  'manifest.json',
+  'results.html',
+  'contact.html',
+  'about.html',
+  'news.html',
+  'icon-192.png',
+  'icon-512.png'
 ];
 
+// تثبيت الكاش
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -16,6 +20,7 @@ self.addEventListener('install', event => {
   );
 });
 
+// تفعيل الكاش
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
